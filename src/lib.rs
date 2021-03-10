c4195697aa3a0a70dde3e11e77b8942406085699
Daniel Franklin <daniel@danielzfranklin.org>
2021-03-10 22:21:45 +0000

    Support checking kernel mod version compatibility

--- src/lib.rs
+++ src/lib.rs
@@ -13,9 +13,9 @@ use std::option::NoneError;
 const MOD_VERSION_FILE: &str = "/sys/devices/evdi/version";
 
 pub struct ModVersion {
-    pub major: i32,
-    pub minor: i32,
-    pub patch: i32
+    pub major: u32,
+    pub minor: u32,
+    pub patch: u32
 }
 
 impl ModVersion {
@@ -43,6 +43,8 @@ pub struct LibVersion {
     pub major: i32,
     pub minor: i32,
     pub patch: i32,
+    compatible_mod_major: u32,
+    compatible_mod_minor: u32,
 }
 
 impl LibVersion {
@@ -74,11 +76,18 @@ impl LibVersion {
         version
     }
 
-    fn new(sys: &evdi_lib_version) -> Self {
+    pub fn is_compatible_with(&self, other: ModVersion) -> bool {
+        // Based on the private function is_evdi_compatible
+        other.major == self.compatible_mod_major && other.minor >= self.compatible_mod_minor
+    }
+
+    fn new(sys: &evdi_lib_version, compatible_mod_major: u32, compatible_mod_minor: u32) -> Self {
         Self {
             major: sys.version_major,
             minor: sys.version_minor,
             patch: sys.version_patchlevel,
+            compatible_mod_major,
+            compatible_mod_minor,
         }
     }
 }
@@ -96,5 +105,12 @@ mod tests {
     fn get_mod_version_works() {
         let result = ModVersion::get();
         assert!(result.is_some())
+    }
+
+    #[test]
+    fn installed_kernel_mod_is_compatible() {
+        let mod_version = ModVersion::get().unwrap();
+        let lib_version = LibVersion::get();
+        assert!(lib_version.is_compatible_with(mod_version));
     }
 }
